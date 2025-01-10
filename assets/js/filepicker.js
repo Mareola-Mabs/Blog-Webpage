c7e1806bdc951c0ed7bef995b6f1f1df1e477797
Ibukunola <bulun1001@gmail.com>
2025-01-10 14:22:18 +0100

    feat: add the profile page

---
+++ assets/js/filepicker.js
@@ -0,0 +1,7 @@
+// For the Proflie Pickture Picker
+let file_btn = document.getElementById('file_btn')
+let file_picker = document.getElementById('file_picker')
+
+file_picker.addEventListener('click', ()=>{
+    file_btn.click()
+})
--- tailwind_files/tailwind.config.js
+++ tailwind_files/tailwind.config.js
@@ -8,10 +8,12 @@ module.exports = {
         'primary-color-a': 'rgba(63, 111, 243, 0.9)',
         'nav-color-a': 'rgba(89, 97, 113, 1.0)',
         'form-bg': 'rgba(248, 249, 252, 1.0)',
+        'form-submit': 'rgba(214, 217, 221, 0.6)',
         'fonts-color': '#7B8CAE'
       },
 
       screens: {
+        'max-2xl': {'max':'1600px'},
         'max-xl': {'max':'1400px'},
         'max-xl1': {'max':'1330px'},
         'max-lg': {'max':'1130px'},
